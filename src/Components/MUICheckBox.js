6abf321b4383a2a25ed7807c58c6eb989837c432
kr-nirmit <nirmit.t@knackroot.com>
2022-01-18 10:57:46 +0530

    add Icon in checkbox

--- src/Components/MUICheckBox.js
+++ src/Components/MUICheckBox.js
@@ -1,16 +1,20 @@
+import { Bookmark, BookmarkBorder } from "@mui/icons-material";
 import { Checkbox } from "@mui/material";
 import React from "react";
 
 function MUICheckBox() {
   return (
     <div>
-      <h1>React Material Ui</h1>
+      <h1>React Material Ui CheckBoxes</h1>
       <div>
         <Checkbox color="success" />
         <Checkbox checked />
         <Checkbox disabled />
         <Checkbox disabled checked />
       </div><hr />
+      <div>
+          <Checkbox icon={<BookmarkBorder />} checkedIcon={<Bookmark />} />
+      </div>
     </div>
   );
 }
